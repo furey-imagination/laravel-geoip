f5b78380a5fdaf315f8e8550fbc6179abc1c9567
James Furey <james.furey@imagination.com>
2015-03-18 13:07:59 +1100

    Config file syntax hotfix.

--- src/config/config.php
+++ src/config/config.php
@@ -6,12 +6,12 @@ return array(
     'service' => 'maxmind',
 
     'maxmind' => array(
-        'type'          => 'database', // database or web_service
+        'type'          => 'database',  // database or web_service
         'user_id'       => '',
         'license_key'   => '',
         'database'      => array(
-            'path'              => ''   // Full path to database, excluding name. If omitted, defaults to 'app_path()."/database/maxmind/"'.
+            'path'              => '',  // Full path to database, excluding name. If omitted, defaults to 'app_path()."/database/maxmind/"'.
             'name'              => '',  // Defaults to "GeoLite2-City.mmdb".
-        }
+        )
     )
 );
